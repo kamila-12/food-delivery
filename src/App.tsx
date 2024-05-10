148231bf6dc0a22a3de41faa0bb8bf389fe7c39a
Kamila <kamieva_k@iuca.kg>
2024-05-11 00:33:30 +0600

    Google authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home';
+import LoginPage from './pages/login';
+import { initializeApp } from "firebase/app";
+import { config } from './config/config';
+import AuthRoute from './components/AuthRoutes';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+initializeApp(config.firebaseConfig);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+export interface IApplicationProps {}
 
+const App: React.FunctionComponent<IApplicationProps> = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <AuthRoute>
+                            <HomePage />
+                        </AuthRoute>
+                    }
+                />
+                <Route path="react-app/login" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 export default App
---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+export const config = {
+  firebaseConfig: {
+    apiKey: "AIzaSyCwGkjeWdE5KQ_7AOv5Z_m1ur9zJurB3T8",
+    authDomain: "login-with-eb594.firebaseapp.com",
+    projectId: "login-with-eb594",
+    storageBucket: "login-with-eb594.appspot.com",
+    messagingSenderId: "230795432127",
+    appId: "1:230795432127:web:0cfed003caf6e4da67f60a",
+    measurementId: "G-WETXRF9XYH"
+
+  }  
+  
+};
