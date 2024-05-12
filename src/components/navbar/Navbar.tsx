953cbadac7455386ee042cd8bb9e0b48a5d417f0
cruelrin <sheveleva_k@iuca.kg>
2024-05-13 02:50:50 +0600

    cart done

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Navbar.css'
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 
 
 const Navbar = () => {
@@ -8,17 +9,20 @@ const Navbar = () => {
 
   const [menu, setMenu] = useState("menu")
 
+  const {getTotalCartAmount} = useContext(StoreContext);
+
+
   return (
     <div className="navbar">
-      <img src={assets.logo} alt = "" className="logo"/>
+      <Link to='/'><img src={assets.logo} alt = "" className="logo"/></Link>
       <ul className="navbar-menu">
         <li onClick = {()=>setMenu("menu")}className= {menu==="menu"? "active":""}>menu</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt=""/>
         <div className="navbar-search-icon">
-          <img src = {assets.basket_icon} alt = "" />
-          <div className="dot"></div>
+          <Link to='/cart'><img src = {assets.basket_icon} alt = "" /> </Link>
+          <div className={getTotalCartAmount()===0?"":dot}></div>
         </div>
         <button>sign in</button>
       </div>
