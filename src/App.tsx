cce5550afecc6b4b09100b470efdb821fce0e0dc
cruelrin <sheveleva_k@iuca.kg>
2024-05-13 00:20:54 +0600

    footer done

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import Footer from './components/Footer/Footer'
 
 
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar/>
       <Routes>
-      <Route path = '/' element = {<Home/>} />
-      
+        <Route path = '/' element = {<Home/>} />
+        <Route path ='/cart' element={<Cart/>} />
       </Routes>
     </div>
+    <Footer />
+    </>
   )
 }
 
